84e12e3f427b5fb1c35e46145c40cf5d3f469c3a
lirh <lironghua@mindmedia.cn>
2016-08-29 17:50:50 +0800

    init

---
+++ some/src/main/java/com/wisely/some/TestController.java
@@ -0,0 +1,24 @@
+package com.wisely.some;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by lrh on 2016/7/27.
+ */
+@RestController
+@RequestMapping(value = "/test")
+@RefreshScope
+public class TestController {
+    @Value("${my.message}") //1
+    private String message;
+
+    @RequestMapping(value = "/getsome")
+    public String getsome(){
+        return message;
+    }
+
+}
